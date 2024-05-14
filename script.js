57113ac034070e90404f56a2b6ccb2245284d2dc
Zachary Jensen <mrzacharyjensen@gmail.com>
2024-05-14 16:29:57 +1200

    Add parsing of text file

--- script.js
+++ script.js
@@ -15,19 +15,101 @@ var load_file = function (filename) {
         }
     };
     xhr.send();
-    console.log(fileContent);
     return fileContent;
 };
+var parse_file = function (file_content) {
+    var lines = file_content.split(/\r?\n/);
+    var recipes = {};
+    var current_category = "";
+    var current_recipe_name = "";
+    var current_recipe = "";
+    for (var i = 0; i < lines.length; i++) {
+        var line = lines[i];
+        // if (line.slice(0,2) == "##") {
+        //   current_category = line.slice(3);
+        // } else if (line.slice(0,2) == "# ") {
+        //   current_recipe_name = line.slice(2);
+        // }
+        switch (line.slice(0, 2)) {
+            case "##":
+                if (current_category != "") {
+                    recipes[current_category][current_recipe_name] = current_recipe;
+                    current_recipe = "";
+                    current_recipe_name = "";
+                }
+                current_category = line.slice(3);
+                recipes[current_category] = {};
+                break;
+            case "# ":
+                if (current_recipe_name != "") {
+                    recipes[current_category][current_recipe_name] = current_recipe;
+                    current_recipe = "";
+                }
+                current_recipe_name = line.slice(2);
+                break;
+            case "":
+                break;
+            default:
+                current_recipe += line + '\n';
+                break;
+        }
+    }
+    recipes[current_category][current_recipe_name] = current_recipe;
+    return recipes;
+};
 var file_content = load_file(filename);
-console.log(file_content);
-var lines = file_content.split(/\r?\n/);
-console.log(lines);
-// let categories: String[] = [];
-// let categories = ["Margaritas", "Cocktails", "Mocktails"];
-var recipes = {};
+var recipes = parse_file(file_content);
+// const lines = file_content.split(/\r?\n/);
+//
+// let recipes = {};
+// let current_category = "";
+// let current_recipe_name = "";
+// let current_recipe = "";
+// for (let i = 0; i < lines.length; i++) {
+//   const line = lines[i];
+//   // if (line.slice(0,2) == "##") {
+//   //   current_category = line.slice(3);
+//   // } else if (line.slice(0,2) == "# ") {
+//   //   current_recipe_name = line.slice(2);
+//   // }
+//   switch (line.slice(0,2))   {
+//     case "##":
+//       if (current_category != "") {
+//         recipes[current_category][current_recipe_name] = current_recipe;
+//         current_recipe = "";
+//         current_recipe_name = "";
+//       }
+//       current_category = line.slice(3);
+//       recipes[current_category] = {};
+//       break;
+//     case "# ":
+//       if (current_recipe_name != "") {
+//         recipes[current_category][current_recipe_name] = current_recipe;
+//         current_recipe = "";
+//       }
+//       current_recipe_name = line.slice(2);
+//       break;
+//     case "":
+//       break;
+//     default:
+//       current_recipe += line + '\n';
+//       break;
+//   }
+// }
+// recipes[current_category][current_recipe_name] = current_recipe;
+console.log(recipes);
 var categoriesElement = document.getElementById("categories");
-var fragment = document.createDocumentFragment();
-var cat = fragment
-    .appendChild(document.createElement("div"));
-cat.textContent = "Thing";
-categoriesElement.appendChild(cat);
+// const fragment = document.createDocumentFragment();
+// const cat = fragment
+//     .appendChild(document.createElement("li"))
+// cat.textContent = "Thing";
+// categoriesElement.appendChild(cat)
+var categories = Object.keys(recipes);
+for (var i = 0; i < categories.length; i++) {
+    var category = categories[i];
+    var fragment = document.createDocumentFragment();
+    var cat_element = fragment
+        .appendChild(document.createElement("li"));
+    cat_element.textContent = category;
+    categoriesElement.appendChild(cat_element);
+}
--- script.ts
+++ script.ts
@@ -18,19 +18,67 @@ const load_file = function(filename: string): String {
   return fileContent;
 }
 
+const parse_file = function(file_content: String): Object {
+  const lines = file_content.split(/\r?\n/);
+  let recipes = {};
+  let current_category = "";
+  let current_recipe_name = "";
+  let current_recipe = "";
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    // if (line.slice(0,2) == "##") {
+    //   current_category = line.slice(3);
+    // } else if (line.slice(0,2) == "# ") {
+    //   current_recipe_name = line.slice(2);
+    // }
+    switch (line.slice(0,2))   {
+      case "##":
+        if (current_category != "") {
+        recipes[current_category][current_recipe_name] = current_recipe;
+        current_recipe = "";
+        current_recipe_name = "";
+      }
+      current_category = line.slice(3);
+      recipes[current_category] = {};
+      break;
+      case "# ":
+        if (current_recipe_name != "") {
+        recipes[current_category][current_recipe_name] = current_recipe;
+        current_recipe = "";
+      }
+      current_recipe_name = line.slice(2);
+      break;
+      case "":
+        break;
+      default:
+        current_recipe += line + '\n';
+      break;
+    }
+  }
+  recipes[current_category][current_recipe_name] = current_recipe;
+  
+  return recipes;
+}
+
 const file_content = load_file(filename);
-const lines = file_content.split(/\r?\n/);
+const recipes = parse_file(file_content);
 
-// let categories: String[] = [];
-
-// let categories = ["Margaritas", "Cocktails", "Mocktails"];
-
-let recipes = {};
-
+console.log(recipes);
 
 var categoriesElement = document.getElementById("categories");
-const fragment = document.createDocumentFragment();
-const cat = fragment
-    .appendChild(document.createElement("div"))
-cat.textContent = "Thing";
-categoriesElement.appendChild(cat)
+// const fragment = document.createDocumentFragment();
+// const cat = fragment
+//     .appendChild(document.createElement("li"))
+// cat.textContent = "Thing";
+// categoriesElement.appendChild(cat)
+
+let categories = Object.keys(recipes);
+for (let i = 0; i < categories.length; i++) {
+  const category = categories[i];
+  
+  const fragment = document.createDocumentFragment();
+  const cat_element = fragment
+    .appendChild(document.createElement("li"))
+  cat_element.textContent = category;
+  categoriesElement.appendChild(cat_element)
+}
